f89c8a48ff263802d0ac3c248a12c5c7c166a4c6
buckethead1986 <rspeckpfs@gmail.com>
2017-10-08 11:41:07 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,34 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
+  @@items = ["Apples","Carrots","Pears", "Figs"] #class variable holding stuff
+  @@cart = []
 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  def call(env) #still havnt had env explained
+    resp = Rack::Response.new #responses to request (basically 'puts')
+    req = Rack::Request.new(env) #requests, basically gets.chomp(, but for urls?)
 
-    if req.path.match(/items/)
+    if req.path.match(/items/) #iteration to resp.write each item in the list
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      search_term = req.params["q"] #backwards hash assignment
+      resp.write handle_search(search_term) #abstracted code, below. finds item if in the list, else resp.writes out a 'nope' message
+
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item_in_cart|
+          resp.write "#{item_in_cart}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+      add_term = req.params["item"]
+      resp.write add_item(add_term)
+
     else
       resp.write "Path Not Found"
     end
@@ -21,10 +37,20 @@ class Application
   end
 
   def handle_search(search_term)
-    if @@items.include?(search_term)
+    if @@items.include?(search_term) #checks @@items to see if the search term in the url matches one of the items.
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
     end
   end
+
+  def add_item(add_term)
+    if @@items.include?(add_term)
+      @@cart << add_term
+      return "added #{add_term}\n"
+    else
+      return "We don't have that item"
+    end
+  end
+
 end
